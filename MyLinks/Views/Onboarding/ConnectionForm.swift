e8efd70874815bf605a3fd9ca8401ea2c9909ed5
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-06-24 14:04:39 +0200

    Adapted onboarding for ipad

--- MyLinks/Views/Onboarding/ConnectionForm.swift
+++ MyLinks/Views/Onboarding/ConnectionForm.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct ConnectionForm: View {
     @EnvironmentObject private var onboardingViewModel: OnboardingViewModel
     
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    
     var body: some View {
         if onboardingViewModel.hostingMode == nil {
             VStack {
@@ -16,93 +18,19 @@ struct ConnectionForm: View {
             }
         }
         else {
-            List {
-                Section {
-                    VStack(alignment: .leading) {
-                        Image(systemName: "list.bullet")
-                            .font(.system(size: 60))
-                        Spacer()
-                            .frame(height: 24)
-                        Text("Setup the server connection")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 30))
+            Group {
+                if horizontalSizeClass == .regular {
+                    List {
+                        ListContent()
+                    }
+                    .padding()
+                    .listStyle(.plain)
+                }
+                else {
+                    List {
+                        ListContent()
                     }
                 }
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                if onboardingViewModel.hostingMode == .selfhosted {
-                    Section("Server route") {
-                        Picker("Connection method", selection: $onboardingViewModel.connectionMethod) {
-                            Text("HTTP")
-                                .tag(Enums.ConnectionMethod.http)
-                            Text("HTTPS")
-                                .tag(Enums.ConnectionMethod.https)
-                        }
-                        TextField("IP address or domain", text: $onboardingViewModel.ipDomain)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                            .keyboardType(.URL)
-                        TextField("Port", text: $onboardingViewModel.port)
-                            .keyboardType(.numberPad)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                        TextField("Path", text: $onboardingViewModel.path)
-                            .keyboardType(.URL)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                    }
-                }
-                Section("Authentication") {
-                    HStack {
-                        SecureField("Token", text: $onboardingViewModel.token)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                        Button {
-                            if let clipboardText = UIPasteboard.general.string {
-                                onboardingViewModel.token = clipboardText
-                            }
-                        } label: {
-                            Image(systemName: "doc.on.clipboard")
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                    }
-                }
-                Section {
-                    Button {
-                        onboardingViewModel.onConnect()
-                    } label: {
-                        Spacer()
-                        if onboardingViewModel.connecting == true {
-                            ProgressView()
-                        }
-                        else {
-                            Text("Connect")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        }
-                        Spacer()
-                    }
-                    .buttonStyle(BorderedProminentButtonStyle())
-                    .padding(.vertical, 6)
-                }
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                Section {
-                    Button {
-                        withAnimation(.default) {
-                            onboardingViewModel.selectedTab = 1
-                        }
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "chevron.left")
-                            Text("Back")
-                            Spacer()
-                        }
-                    }
-                }
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
             .disabled(onboardingViewModel.connecting)
             .alert("Invalid values", isPresented: $onboardingViewModel.invalidValuesAlert, actions: {
@@ -124,5 +52,101 @@ struct ConnectionForm: View {
                 Text(onboardingViewModel.connectionErrorMessage)
             })
         }
+    }
+}
+
+private struct ListContent: View {
+    @EnvironmentObject private var onboardingViewModel: OnboardingViewModel
+    
+    var body: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Image(systemName: "list.bullet")
+                    .font(.system(size: 60))
+                Spacer()
+                    .frame(height: 24)
+                Text("Setup the server connection")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 30))
+            }
+        }
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+        if onboardingViewModel.hostingMode == .selfhosted {
+            Section("Server route") {
+                Picker("Connection method", selection: $onboardingViewModel.connectionMethod) {
+                    Text("HTTP")
+                        .tag(Enums.ConnectionMethod.http)
+                    Text("HTTPS")
+                        .tag(Enums.ConnectionMethod.https)
+                }
+                TextField("IP address or domain", text: $onboardingViewModel.ipDomain)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                    .keyboardType(.URL)
+                TextField("Port", text: $onboardingViewModel.port)
+                    .keyboardType(.numberPad)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                TextField("Path", text: $onboardingViewModel.path)
+                    .keyboardType(.URL)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+            }
+        }
+        Section("Authentication") {
+            HStack {
+                SecureField("Token", text: $onboardingViewModel.token)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                Button {
+                    if let clipboardText = UIPasteboard.general.string {
+                        onboardingViewModel.token = clipboardText
+                    }
+                } label: {
+                    Image(systemName: "doc.on.clipboard")
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+        }
+        Section {
+            Button {
+                onboardingViewModel.onConnect()
+            } label: {
+                Spacer()
+                if onboardingViewModel.connecting == true {
+                    ProgressView()
+                }
+                else {
+                    Text("Connect")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 18))
+                }
+                Spacer()
+            }
+            .buttonStyle(BorderedProminentButtonStyle())
+            .padding(.vertical, 6)
+        }
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+        Section {
+            Button {
+                withAnimation(.default) {
+                    onboardingViewModel.selectedTab = 1
+                }
+            } label: {
+                HStack {
+                    Spacer()
+                    Image(systemName: "chevron.left")
+                    Text("Back")
+                    Spacer()
+                }
+            }
+        }
+        .listRowSeparator(.hidden)
+        .listRowBackground(Color.clear)
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
     }
 }
--- MyLinks/Views/Onboarding/OnboardingView.swift
+++ MyLinks/Views/Onboarding/OnboardingView.swift
@@ -3,21 +3,59 @@ import SwiftUI
 struct OnboardingView: View {
     @EnvironmentObject private var onboardingViewModel: OnboardingViewModel
     
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.colorScheme) private var colorScheme
+    
     var body: some View {
-        TabView(selection: $onboardingViewModel.selectedTab) {
-            Welcome()
-                .tag(0)
-                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
-            ServerSelection()
-                .tag(1)
-                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
-            ConnectionForm()
-                .tag(2)
-                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+        if horizontalSizeClass == .compact {
+            TabView(selection: $onboardingViewModel.selectedTab) {
+                Welcome()
+                    .tag(0)
+                    .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                ServerSelection()
+                    .tag(1)
+                    .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                ConnectionForm()
+                    .tag(2)
+                    .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+            }
+            .background(Color.listBackground)
+            .fontDesign(.rounded)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+            .contentShape(Rectangle()).simultaneousGesture(DragGesture())
         }
-        .background(Color.listBackground)
-        .fontDesign(.rounded)
-        .tabViewStyle(.page(indexDisplayMode: .never))
-        .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+        else {
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Group {
+                        TabView(selection: $onboardingViewModel.selectedTab) {
+                            Welcome()
+                                .tag(0)
+                                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                            ServerSelection()
+                                .tag(1)
+                                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                            ConnectionForm()
+                                .tag(2)
+                                .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                        }
+                        .padding()
+                        .background(colorScheme == .dark ? Color.black : Color.white)
+                        .fontDesign(.rounded)
+                        .tabViewStyle(.page(indexDisplayMode: .never))
+                        .contentShape(Rectangle()).simultaneousGesture(DragGesture())
+                    }
+                    .frame(maxWidth: 600, maxHeight: 800)
+                    .cornerRadius(12)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .background(Color.listBackground)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .ignoresSafeArea()
+        }
     }
 }
