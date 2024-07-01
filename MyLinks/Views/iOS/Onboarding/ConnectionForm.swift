18176013b1938d3e2ac919abd705e43a1481e1fb
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-07-01 20:24:03 +0200

    Fixes

--- MyLinks/Views/iOS/Onboarding/ConnectionForm.swift
+++ MyLinks/Views/iOS/Onboarding/ConnectionForm.swift
@@ -8,140 +8,127 @@ struct ConnectionForm: View {
     @State private var tokenInstructionsSheet = false
     
     var body: some View {
-        if onboardingViewModel.hostingMode == nil {
-            VStack {
-                Spacer()
-                Button("Back to hosting mode selection") {
+        Form {
+            Section {
+                VStack(alignment: .leading) {
+                    Image(systemName: "list.bullet")
+                        .font(.system(size: 60))
+                    Spacer()
+                        .frame(height: 24)
+                    Text("Setup the server connection")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 30))
+                }
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.clear)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            if onboardingViewModel.hostingMode == .selfhosted {
+                Section("Server route") {
+                    Picker("Connection method", selection: $onboardingViewModel.connectionMethod) {
+                        Text("HTTP")
+                            .tag(Enums.ConnectionMethod.http)
+                        Text("HTTPS")
+                            .tag(Enums.ConnectionMethod.https)
+                    }
+                    TextField("IP address or domain", text: $onboardingViewModel.ipDomain)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                        .keyboardType(.URL)
+                    TextField("Port", text: $onboardingViewModel.port)
+                        .keyboardType(.numberPad)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                    TextField("Path", text: $onboardingViewModel.path)
+                        .keyboardType(.URL)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+            }
+            Section {
+                HStack {
+                    SecureField("Token", text: $onboardingViewModel.token)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                    Button {
+                        if let clipboardText = UIPasteboard.general.string {
+                            onboardingViewModel.token = clipboardText
+                        }
+                    } label: {
+                        Image(systemName: "doc.on.clipboard")
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            } header: {
+                Text("Authentication")
+            } footer: {
+                Button("How to get an API token") {
+                    tokenInstructionsSheet = true
+                }
+                .font(.system(size: 12))
+            }
+            Section {
+                Button {
+                    onboardingViewModel.onConnect()
+                } label: {
+                    Spacer()
+                    if onboardingViewModel.connecting == true {
+                        ProgressView()
+                    }
+                    else {
+                        Text("Connect")
+                            .fontWeight(.semibold)
+                            .font(.system(size: 18))
+                    }
+                    Spacer()
+                }
+                .buttonStyle(BorderedProminentButtonStyle())
+                .padding(.vertical, 6)
+            }
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.clear)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            Section {
+                Button {
                     withAnimation(.default) {
                         onboardingViewModel.selectedTab = 1
                     }
-                }
-                Spacer()
-            }
-        }
-        else {
-            Form {
-                Section {
-                    VStack(alignment: .leading) {
-                        Image(systemName: "list.bullet")
-                            .font(.system(size: 60))
-                        Spacer()
-                            .frame(height: 24)
-                        Text("Setup the server connection")
-                            .fontWeight(.semibold)
-                            .font(.system(size: 30))
-                    }
-                }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                if onboardingViewModel.hostingMode == .selfhosted {
-                    Section("Server route") {
-                        Picker("Connection method", selection: $onboardingViewModel.connectionMethod) {
-                            Text("HTTP")
-                                .tag(Enums.ConnectionMethod.http)
-                            Text("HTTPS")
-                                .tag(Enums.ConnectionMethod.https)
-                        }
-                        TextField("IP address or domain", text: $onboardingViewModel.ipDomain)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                            .keyboardType(.URL)
-                        TextField("Port", text: $onboardingViewModel.port)
-                            .keyboardType(.numberPad)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                        TextField("Path", text: $onboardingViewModel.path)
-                            .keyboardType(.URL)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                    }
-                }
-                Section {
+                } label: {
                     HStack {
-                        SecureField("Token", text: $onboardingViewModel.token)
-                            .autocorrectionDisabled()
-                            .textInputAutocapitalization(.never)
-                        Button {
-                            if let clipboardText = UIPasteboard.general.string {
-                                onboardingViewModel.token = clipboardText
-                            }
-                        } label: {
-                            Image(systemName: "doc.on.clipboard")
-                        }
-                        .buttonStyle(PlainButtonStyle())
-                    }
-                } header: {
-                    Text("Authentication")
-                } footer: {
-                    Button("How to get an API token") {
-                        tokenInstructionsSheet = true
-                    }
-                    .font(.system(size: 12))
-                }
-                Section {
-                    Button {
-                        onboardingViewModel.onConnect()
-                    } label: {
                         Spacer()
-                        if onboardingViewModel.connecting == true {
-                            ProgressView()
-                        }
-                        else {
-                            Text("Connect")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        }
+                        Image(systemName: "chevron.left")
+                        Text("Back")
                         Spacer()
                     }
-                    .buttonStyle(BorderedProminentButtonStyle())
-                    .padding(.vertical, 6)
                 }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                Section {
-                    Button {
-                        withAnimation(.default) {
-                            onboardingViewModel.selectedTab = 1
-                        }
-                    } label: {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "chevron.left")
-                            Text("Back")
-                            Spacer()
-                        }
-                    }
-                }
-                .listRowSeparator(.hidden)
-                .listRowBackground(Color.clear)
-                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
-            .disabled(onboardingViewModel.connecting)
-            .alert("Invalid values", isPresented: $onboardingViewModel.invalidValuesAlert, actions: {
-                Button {
-                    onboardingViewModel.invalidValuesAlert.toggle()
-                } label: {
-                    Text("Close")
-                }
-            }, message: {
-                Text(onboardingViewModel.invalidValuesMessage)
-            })
-            .alert("Connection error", isPresented: $onboardingViewModel.connectionErrorAlert, actions: {
-                Button {
-                    onboardingViewModel.connectionErrorAlert.toggle()
-                } label: {
-                    Text("Close")
-                }
-            }, message: {
-                Text(onboardingViewModel.connectionErrorMessage)
-            })
-            .sheet(isPresented: $tokenInstructionsSheet, content: {
-                TokenInstructions {
-                    tokenInstructionsSheet = false
-                }
-            })
+            .listRowSeparator(.hidden)
+            .listRowBackground(Color.clear)
+            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         }
+        .disabled(onboardingViewModel.connecting)
+        .alert("Invalid values", isPresented: $onboardingViewModel.invalidValuesAlert, actions: {
+            Button {
+                onboardingViewModel.invalidValuesAlert.toggle()
+            } label: {
+                Text("Close")
+            }
+        }, message: {
+            Text(onboardingViewModel.invalidValuesMessage)
+        })
+        .alert("Connection error", isPresented: $onboardingViewModel.connectionErrorAlert, actions: {
+            Button {
+                onboardingViewModel.connectionErrorAlert.toggle()
+            } label: {
+                Text("Close")
+            }
+        }, message: {
+            Text(onboardingViewModel.connectionErrorMessage)
+        })
+        .sheet(isPresented: $tokenInstructionsSheet, content: {
+            TokenInstructions {
+                tokenInstructionsSheet = false
+            }
+        })
     }
 }
