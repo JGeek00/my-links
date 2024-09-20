9e5cdd60d9084e16e78f36ea962ecc4ec7457497
Juan Gilsanz Polo <juangilsanz@gmail.com>
2024-09-20 03:45:35 +0200

    Override SSL validation

--- AddButtonWidget/AddButtonWidgetBundle.swift
+++
@@ -1,9 +0,0 @@
-import WidgetKit
-import SwiftUI
-
-@main
-struct AddButtonWidgetBundle: WidgetBundle {
-    var body: some Widget {
-        AddButtonWidgetControl()
-    }
-}
---
+++ MyLinks/Utils/SSLIgnoringDelegate.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class SSLIgnoringDelegate: NSObject, URLSessionDelegate {
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+    }
+}
