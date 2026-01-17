75dc50eab05e68f4cf6b22eb7ef3fcdf3fa217b0
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-01-17 20:30:04 +0100

    Accept more types of TLD on domains

--- MyLinks/Constants/Regexps.swift
+++ MyLinks/Constants/Regexps.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Regexps {
     public static let ipAddress = #"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$"#
-    public static let domain =  #"^(([a-z0-9|-]+\.)*[a-z0-9|-]+\.[a-z]+)$"#
+    public static let domain =  #"^(([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.[a-zA-Z]{2,})$"#
     public static let path = #"^\/\b([A-Za-z0-9_\-~/]*)[^\/|\.|\:]$"#
-    public static let url = #"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#
+    public static let url = #"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,63}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#
 }
