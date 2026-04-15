d455f12674198a96453a49e621614656f0dd9c1f
Juan Gilsanz Polo <juangilsanz@gmail.com>
2026-04-15 14:06:42 +0200

    Fix delete tag response model

--- MyLinks/Models/DeleteTagResponse.swift
+++ MyLinks/Models/DeleteTagResponse.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // MARK: - DeleteTagResponse
 struct DeleteTagResponse: Codable, Hashable {
-    let response: [DeleteTagResponse_Tag]
+    let response: DeleteTagResponse_Tag
 }
 
 // MARK: - DeleteTagResponse_Tag
@@ -12,18 +12,11 @@ struct DeleteTagResponse_Tag: Codable, Hashable {
     let ownerID: Int
     let aiGenerated: Bool
     let createdAt, updatedAt: String
-    let count: DeleteTagResponse_Tag_Count
 
     enum CodingKeys: String, CodingKey {
         case id, name
         case ownerID = "ownerId"
         case aiGenerated, createdAt, updatedAt
-        case count = "_count"
     }
-}
-
-// MARK: - DeleteTagResponse_Tag_Count
-struct DeleteTagResponse_Tag_Count: Codable, Hashable {
-    let links: Int
 }
 
